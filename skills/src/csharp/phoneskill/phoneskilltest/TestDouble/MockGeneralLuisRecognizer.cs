80f08289c6d7938b9613c636963fa43cec078514
Marion Zepf <mazepf@microsoft.com>
2019-02-22 12:04:00 -0500

    [WIP] Phone: New skill to make phone calls

---
+++ skills/src/csharp/phoneskill/phoneskilltest/TestDouble/MockGeneralLuisRecognizer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Luis;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.AI.Luis;
+using Microsoft.Bot.Builder.Dialogs;
+using static Luis.General;
+
+namespace PhoneSkillTest.TestDouble
+{
+    public class MockGeneralLuisRecognizer : ITelemetryRecognizer
+    {
+        public MockGeneralLuisRecognizer()
+        {
+        }
+
+        public bool LogPersonalInformation { get; set; } = false;
+
+        public IBotTelemetryClient TelemetryClient { get; set; } = new NullBotTelemetryClient();
+
+        public Task<RecognizerResult> RecognizeAsync(ITurnContext turnContext, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> RecognizeAsync<T>(ITurnContext turnContext, CancellationToken cancellationToken)
+            where T : IRecognizerConvert, new()
+        {
+            var mockGeneral = new General
+            {
+                Intents = new Dictionary<Intent, IntentScore>()
+            };
+            mockGeneral.Intents.Add(Intent.None, new IntentScore() { Score = 1.0 });
+
+            var test = mockGeneral as object;
+            var mockResult = (T)test;
+
+            return Task.FromResult(mockResult);
+        }
+
+        public Task<T> RecognizeAsync<T>(DialogContext dialogContext, CancellationToken cancellationToken = default(CancellationToken))
+            where T : IRecognizerConvert, new()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RecognizerResult> RecognizeAsync(ITurnContext turnContext, Dictionary<string, string> telemetryProperties, Dictionary<string, double> telemetryMetrics, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> RecognizeAsync<T>(ITurnContext turnContext, Dictionary<string, string> telemetryProperties, Dictionary<string, double> telemetryMetrics, CancellationToken cancellationToken = default(CancellationToken))
+            where T : IRecognizerConvert, new()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ skills/src/csharp/phoneskill/phoneskilltest/TestDouble/MockPhoneLuisRecognizer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Luis;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.AI.Luis;
+using Microsoft.Bot.Builder.Dialogs;
+using PhoneSkillTest.Flow.Utterances;
+using static Luis.PhoneLuis;
+
+namespace PhoneSkillTest.TestDouble
+{
+    public class MockPhoneLuisRecognizer : ITelemetryRecognizer
+    {
+        private OutgoingCallUtterances phoneUtterancesManager;
+
+        public MockPhoneLuisRecognizer()
+        {
+            this.phoneUtterancesManager = new OutgoingCallUtterances();
+        }
+
+        public bool LogPersonalInformation { get; set; } = false;
+
+        public IBotTelemetryClient TelemetryClient { get; set; } = new NullBotTelemetryClient();
+
+        public Task<RecognizerResult> RecognizeAsync(ITurnContext turnContext, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> RecognizeAsync<T>(ITurnContext turnContext, CancellationToken cancellationToken)
+            where T : IRecognizerConvert, new()
+        {
+            var intent = new PhoneLuis
+            {
+                Intents = new Dictionary<Intent, IntentScore>()
+            };
+            intent.Intents.Add(Intent.None, new IntentScore() { Score = 1.0 });
+
+            var test = intent as object;
+            var mockResult = (T)test;
+
+            return Task.FromResult(mockResult);
+        }
+
+        public Task<T> RecognizeAsync<T>(DialogContext dialogContext, CancellationToken cancellationToken = default(CancellationToken))
+            where T : IRecognizerConvert, new()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RecognizerResult> RecognizeAsync(ITurnContext turnContext, Dictionary<string, string> telemetryProperties, Dictionary<string, double> telemetryMetrics, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> RecognizeAsync<T>(ITurnContext turnContext, Dictionary<string, string> telemetryProperties, Dictionary<string, double> telemetryMetrics, CancellationToken cancellationToken = default(CancellationToken))
+            where T : IRecognizerConvert, new()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
